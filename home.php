44a85479d615d3fac4c091f149bbadb8c4572819
Сергей Субботин <zarodiny@yandex.ru>
2025-07-18 16:41:03 +1000

    feat: добавлена анимация для изображений в секции "О нас"

--- home.php
+++ home.php
@@ -65,7 +65,7 @@ get_header();
             </div>
             <div class="about-section__wrapper">
                 <div class="about-section__item">
-                    <div class="image-wrapper">
+                    <div class="image-wrapper image-wrapper-animate">
                         <img width="704" height="685" src="<?php echo get_template_directory_uri(); ?>/img/about-1.webp" alt="о нас">
                     </div>
                 </div>
