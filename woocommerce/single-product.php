dbc23b4567b153577ea087380c8bb0491712cd08
Сергей Субботин <zarodiny@yandex.ru>
2025-07-22 10:14:37 +1000

    feat: добавлены стили для страницы товара, улучшена адаптивность и добавлен контейнер для карточки товара

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header( 'shop' ); ?>
-
+<div class="container single-product-card">
 	<?php
 		/**
 		 * woocommerce_before_main_content hook.
@@ -55,6 +55,8 @@ get_header( 'shop' ); ?>
 		 */
 		do_action( 'woocommerce_sidebar' );
 	?>
+
+	</div>
 
 <?php
 get_footer( 'shop' );
